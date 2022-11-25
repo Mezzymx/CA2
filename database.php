849b59c01c0883dab6b889583de0be59a70db3b5
Esther <esthermampuya@gmail.com>
2022-11-25 01:48:43 +0000

    header change

--- database.php
+++ database.php
@@ -3,6 +3,7 @@
     $username = 'root';
     $password = '';
 
+
     try {
         $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
--- index.php
+++ index.php
@@ -42,7 +42,7 @@ $statement3->closeCursor();
 <?php
 include('includes/header.php');
 ?>
-<h1>Record List</h1>
+<h1>Pet Paradise Hotel</h1>
 <aside>
 <!-- display a list of categories -->
 <h2>Categories</h2>
